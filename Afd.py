59cabf0999fcaccdd7f418a3ada287d3f5f07ab3
Isabela Chacon <146193311+BelaChacon@users.noreply.github.com>
2024-10-07 22:37:12 -0300

    Update Afd.py

--- Afd.py
+++ Afd.py
@@ -4,23 +4,23 @@ class Afd:
         self.estado_atual = 'q0'  #Estado inicial do autômato
     
     #Define as transições de estado
-    def transicao(self, simbolo):
+    def transicao(self, char):
         if self.estado_atual == 'q0':
-            if simbolo == '1':
+            if char == '1':
                 self.estado_atual = 'q1'
         elif self.estado_atual == 'q1':
-            if simbolo == '0':
+            if char == '0':
                 self.estado_atual = 'q0'
     
     #Analisa o conteudo de entrada e verifica se é aceita
-    def reconhece(self, cadeia):
+    def reconhece(self, string):
         self.estado_atual = 'q0'
         
-        for simbolo in cadeia:
-            if simbolo not in ['0', '1']:
-                print(f"Símbolo inválido encontrado: {simbolo}")
+        for char in string:
+            if char not in ['0', '1']:
+                print(f"Caractere inválido: {char}")
                 return False
-            self.transicao(simbolo)
+            self.transicao(char)
         
         #O autômato aceita a cadeia se termina no estado 'q1'
         return self.estado_atual == 'q1'
@@ -28,10 +28,10 @@ class Afd:
 afd = Afd()
 
 #Teste
-cadeias = ['110', '101', '1000', '1111', '001', '000', '1', '0']
+strings = ['110', '101', '1000', '1111', '001', '000', '1', '0']
 
-for cadeia in cadeias:
-    if afd.reconhece(cadeia):
-        print(f"A cadeia '{cadeia}' foi aceita.")
+for string in strings:
+    if afd.reconhece(string):
+        print(f"A cadeia '{string}' foi aceita.")
     else:
-        print(f"A cadeia '{cadeia}' foi rejeitada.")
+        print(f"A cadeia '{string}' foi rejeitada.")
