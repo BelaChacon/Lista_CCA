6fbc53bb96fee995ea3d79c00847536f5eda432a
Isabela Chacon <146193311+BelaChacon@users.noreply.github.com>
2024-10-08 10:51:34 -0300

    Create AfnZeroSeguidoUm.py

---
+++ AfnZeroSeguidoUm.py
@@ -0,0 +1,34 @@
+#questão 10 da lista
+
+class AfnZeroSeguidoUm:
+    def __init__(self):
+        self.estado_inicial = 'q0'  #Estado inicial
+        self.estado_aceitacao = 'q2'  #Estado de aceitação
+        self.estado_atual = self.estado_inicial  
+    
+    def transicao(self, char):
+        if self.estado_atual == 'q0':
+            if char == '0':
+                self.estado_atual = 'q1'  
+        elif self.estado_atual == 'q1':
+            if char == '0':
+                self.estado_atual = 'q1' 
+            elif char == '1':
+                self.estado_atual = 'q2'  
+        elif self.estado_atual == 'q2':
+            pass 
+
+    def reconhece(self, string):
+        self.estado_atual = self.estado_inicial  
+        for char in string:
+            self.transicao(char)
+        return self.estado_atual == self.estado_aceitacao
+
+afn = AfnZeroSeguidoUm()
+
+#Teste
+strings = ['01', '001', '100', '0001', '110', '111', '000', '10', '011', '0101', '1001']
+
+for string in strings:
+    resultado = afn.reconhece(string)
+    print(f"A string '{string}' possui um zero seguido de pelo menos um número 1? {resultado}")
