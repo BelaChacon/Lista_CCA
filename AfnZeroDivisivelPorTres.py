7de8488414043fb5ac80d3f1c0ebfa0d1bd76745
Isabela Chacon <146193311+BelaChacon@users.noreply.github.com>
2024-10-08 17:12:56 -0300

    Update AfnZeroDivisivelPorTres.py

--- AfnZeroDivisivelPorTres.py
+++ AfnZeroDivisivelPorTres.py
@@ -4,36 +4,36 @@ class AfnZeroDivisivelPorTres:
     def __init__(self):
         self.estado_inicial = 'q0'
         self.estados_aceitacao = {'q0'}
-        self.estado_atual = {self.estado_inicial}
     
     def transicao(self, char):
-        novos_estados = set()
-        for estado in self.estado_atual:
-            if estado == 'q0':
-                if char == '0':
-                    novos_estados.add('q1') 
-                novos_estados.add('q0') 
-            elif estado == 'q1':
-                if char == '0':
-                    novos_estados.add('q2') 
-                novos_estados.add('q1') 
-            elif estado == 'q2':
-                if char == '0':
-                    novos_estados.add('q0') 
-                novos_estados.add('q2')  
-        self.estado_atual = novos_estados
+        if self.estado_atual == 'q0':
+            if char == '0':
+                self.estado_atual = 'q1'
+            elif char == '1':
+                self.estado_atual = 'q0'
+        elif self.estado_atual == 'q1':
+            if char == '0':
+                self.estado_atual = 'q2'
+            elif char == '1':
+                self.estado_atual = 'q1'
+        elif self.estado_atual == 'q2':
+            if char == '0':
+                self.estado_atual = 'q0'
+            elif char == '1':
+                self.estado_atual = 'q2'
     
     def reconhece(self, string):
-        self.estado_atual = {self.estado_inicial}
+        self.estado_atual = 'q0'
         for char in string:
             self.transicao(char)
-        return any(estado in self.estados_aceitacao for estado in self.estado_atual)
+
+        return self.estado_atual in self.estados_aceitacao
 
 afn = AfnZeroDivisivelPorTres()
 
 #Teste
-strings = ['0', '00', '000', '0000', '00000', '000000', '111', '010101', '001100', '10101', '000111']
+strings = ['0', '00', '000', '0000', '00000', '000000', '010101', '001100', '10101', '000111']
 
 for string in strings:
     resultado = afn.reconhece(string)
-    print(f"A string '{string}' possui uma quantidade de zeros divisivel por três? {resultado}")
+    print(f"A string '{string}' possui uma quantidade de zero divisivel por 3? {resultado}")
