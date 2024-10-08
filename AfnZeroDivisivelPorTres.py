ebea88e24347664d7238aa9306b9e3a3e8914570
Isabela Chacon <146193311+BelaChacon@users.noreply.github.com>
2024-10-08 10:32:50 -0300

    Create AfnZeroDivisivelPorTres.py

---
+++ AfnZeroDivisivelPorTres.py
@@ -0,0 +1,39 @@
+#questão 8 da lista 
+
+class AfnZeroDivisivelPorTres:
+    def __init__(self):
+        self.estado_inicial = 'q0'
+        self.estados_aceitacao = {'q0'}
+        self.estado_atual = {self.estado_inicial}
+    
+    def transicao(self, char):
+        novos_estados = set()
+        for estado in self.estado_atual:
+            if estado == 'q0':
+                if char == '0':
+                    novos_estados.add('q1') 
+                novos_estados.add('q0') 
+            elif estado == 'q1':
+                if char == '0':
+                    novos_estados.add('q2') 
+                novos_estados.add('q1') 
+            elif estado == 'q2':
+                if char == '0':
+                    novos_estados.add('q0') 
+                novos_estados.add('q2')  
+        self.estado_atual = novos_estados
+    
+    def reconhece(self, string):
+        self.estado_atual = {self.estado_inicial}
+        for char in string:
+            self.transicao(char)
+        return any(estado in self.estados_aceitacao for estado in self.estado_atual)
+
+afn = AfnZeroDivisivelPorTres()
+
+#Teste
+strings = ['0', '00', '000', '0000', '00000', '000000', '111', '010101', '001100', '10101', '000111']
+
+for string in strings:
+    resultado = afn.reconhece(string)
+    print(f"String '{string}' é aceita pelo AFN: {resultado}")
